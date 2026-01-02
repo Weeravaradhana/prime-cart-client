2825e32faa9b71165acec916c2dcd945bf45677d
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-02 21:41:34 +0530

    Add Keycloak authentication configuration and HTTP client with interceptors to Angular appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,46 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import {AbstractSecurityStorage, authInterceptor, LogLevel, provideAuth} from 'angular-auth-oidc-client';
+import {environment} from '../environment/environment';
+import {provideQueryClient, QueryClient} from '@tanstack/angular-query-experimental';
+import {SsrStorageService} from './service/ssr-storage.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideClientHydration(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+
+    provideRouter(routes, withComponentInputBinding()),
+
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor()])
+    ),
+
+    provideAuth({
+      config: {
+        authority: environment.keycloak.authority,
+        redirectUrl: environment.keycloak.redirectUrl,
+        postLogoutRedirectUri: environment.keycloak.postLogoutRedirectUri,
+        clientId: environment.keycloak.clientId,
+
+        scope: 'openid profile email offline_access',
+        responseType: 'code',
+
+        silentRenew: true,
+        useRefreshToken: true,
+
+        secureRoutes: [environment.apiUrl],
+        logLevel: LogLevel.Warn
+      },
+    }),
+
+    { provide: AbstractSecurityStorage, useClass: SsrStorageService },
+
+    provideQueryClient(new QueryClient()),
+  ],
 };
