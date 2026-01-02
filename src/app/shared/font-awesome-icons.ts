26a153bd22e9993a764dc25b56f840e07845848d
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-02 13:26:09 +0530

    feat: define and export Font Awesome icons array

---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,27 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import {
+  faCartShopping,
+  faCheck, faCircleCheck,
+  faShoppingCart,
+  faTruckFast,
+  faUser,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faXmark,
+  faCheck,
+  faShoppingCart,
+  faCircleCheck,
+];
