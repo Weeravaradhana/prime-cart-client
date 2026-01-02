10afbe1da0fe11b46028b9e86eca254e08e7378f
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-02 13:25:15 +0530

    integrate Font Awesome icons with standalone AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './layout/navbar/navbar.component';
+import {FaConfig, FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fontAwesomeIcons} from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prime-cart-client';
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig) ;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+        this.initFontAwesome();
+    }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
